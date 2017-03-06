33ce692a12c5a2960a51e6f32cff22536152fdf4
Michael Cavalea <callmecavs@gmail.com>
2017-03-05 22:30:23 -0500

    commenting

--- src/one-ready.js
+++ src/one-ready.js
@@ -1,13 +1,17 @@
+// returns a Promise that resolves when the DOM is ready
 const ready = () => new Promise(resolve => {
   const { readyState } = document
 
+  // if DOM is already ready, resolve early
   if (readyState === 'interactive' || readyState === 'complete') {
     return resolve()
   }
 
+  // otherwise, resolve when ready
   document.addEventListener('DOMContentLoaded', resolve)
 })
 
+// export the Promise, not the generator
 const singleton = ready()
 
 export default singleton
